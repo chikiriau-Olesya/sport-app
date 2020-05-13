a91301c3367cd690b76452ab10f05051464c5f65
chikiriau-Olesya <faceremirabilia@gmail.com>
2020-05-13 14:39:59 +0300

    add one sketch

--- js/sketch.js
+++ js/sketch.js
@@ -1,26 +1,27 @@
-function setup() {
-    // Создаем канвас, как весь браузер
-    let myCanvas = createCanvas(windowWidth, windowHeight);
-    // прикрепляем канвас в заранее созданный элемент
-    myCanvas.parent("canvas");
-    background(255);
-}
+// function setup() {
+//     // Создаем канвас, как весь браузер
+//     let myCanvas = createCanvas(windowWidth, windowHeight);
+//     // прикрепляем канвас в заранее созданный элемент
+//     myCanvas.parent("canvas");
+//     background(255);
+// }
 
-function draw() {
-    fill(255, 15);
-    noStroke();
-    rect(0, 0, windowWidth, windowHeight);
+// function draw() {
+//     fill(255, 15);
+//     noStroke();
+//     rect(0, 0, windowWidth, windowHeight);
 
-    stroke(0);
-    if (mouseIsPressed) {
-        fill(0);
-    } else {
-        fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
-}
+//     stroke(0);
+//     if (mouseIsPressed) {
+//         fill(0);
+//     } else {
+//         fill(255);
+//     }
+//     ellipse(mouseX, mouseY, 80, 80);
+// }
 
-// Изменяем размер канваса, если изменился размер окна
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
+// // Изменяем размер канваса, если изменился размер окна
+// function windowResized() {
+//     resizeCanvas(windowWidth, windowHeight);
+// }
+
---
+++ js/sketchOne.js
@@ -0,0 +1,83 @@
+let ball = {
+    x: 100,
+    y: 100,
+    speedx: 2,
+    speedy: 3,
+  }
+  
+  function setup() {
+    createCanvas(windowWidth, windowHeight);
+  }
+  
+  function draw() {
+    background('#9EFF00');
+    noStroke();
+
+    //Draw bounce ball with functions
+    drawBall();
+    move();
+    bounce();
+    
+    //draw star
+    translate(width * 0.5, height * 0.5);
+    rotate(frameCount / 50.0);
+    changeShapeFill();
+    noStroke();
+    star(mouseX - 200, mouseY - 200, 60, 100, 40);
+    
+    //draw rect
+    noStroke();
+    rotate(frameCount / 500);
+    rect(-10, 400, 400, 300)
+    
+    changeShapeFill();
+
+  }
+  
+  //Function draws star
+  function star(x, y, radius1, radius2, npoints) {
+    let angle = TWO_PI / npoints;
+    let halfAngle = angle / 2.0;
+    beginShape();
+    for (let i = 0; i < TWO_PI; i += angle) {
+      let sx = x + cos(i) * radius2;
+      let sy = y + sin(i) * radius2;
+      vertex(sx, sy);
+      sx = x + cos(i + halfAngle) * radius1;
+      sy = y + sin(i + halfAngle) * radius1;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
+  
+  //Functions for bounce ball
+  
+  function drawBall() {
+    for(let i = 0; i < 5; i++) {
+      ellipse(ball.x, ball.y, 60, 60)
+    }
+  }
+  function move() {
+    ball.x = ball.x + ball.speedx
+    ball.y = ball.y + ball.speedy
+  }
+  function bounce() {
+    if (ball.x > width || ball.x < 0) {
+      ball.speedx = ball.speedx * -1
+    } 
+    if (ball.y > height || ball.y < 0) {
+      ball.speedy = ball.speedy * -1
+    }
+  
+  }
+  
+  function changeShapeFill() {
+      shapeColor = map(mouseY, mouseX, 0, 255, 200);
+      fill(shapeColor);
+      stroke(shapeColor)
+  }
+  
+  
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
